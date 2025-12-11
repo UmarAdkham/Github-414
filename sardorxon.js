56ad381f30d5399663442e9606137db2f1131bc9
Sardorxon Valiyev <sardorxonvvaliyev006@gmail.com>
2025-12-11 12:29:35 +0500

    Update sardorxon.js

--- sardorxon.js
+++ sardorxon.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-console.log("Valiyev Sardorxon");
-console.log("Test Conflict")
-console.log("Salomaaat")
-=======
-console.log("Salomaaat")
-console.log("Valiyev Sardorxon");
-console.log("Test Conflict")
->>>>>>> 5f8b830771868aca302cffa8fdc36621bc0bce56
+console.log("2*2=4");
